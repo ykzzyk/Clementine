465ba77e3f71128c457208e0196f360634b663a5
ykzzyk <nz9024295@gmail.com>
2026-01-02 13:38:21 -0600

    home screen v0

--- components/ScreenBackground.js
+++ components/ScreenBackground.js
@@ -6,12 +6,12 @@ export default function ScreenBackground({ children }) {
   return (
     <ScrollView style={styles.screenRoot} contentContainerStyle={styles.scroll}>
       <View pointerEvents="none" style={styles.backgroundLayer}>
-        <LinearGradient
+        {/* <LinearGradient
           colors={['#FFF6E5', '#F5F7FF', '#E7F6FF']}
           start={{ x: 0.1, y: 0.1 }}
           end={{ x: 0.9, y: 0.9 }}
           style={StyleSheet.absoluteFill}
-        />
+        /> */}
         <View style={styles.glowTop} />
         <View style={styles.glowBottom} />
       </View>
